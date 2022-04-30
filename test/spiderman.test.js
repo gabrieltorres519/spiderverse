ea68b5a86f9c3d6e6c776851af25f2fc43dd72b7
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-04-30 12:22:34 -0500

    Pasando la prueba luego de resolver todos los errores, ejemplificando TDD

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -19,7 +19,7 @@ describe("Unit test for spiderman class", () => {
       expect(andrewGarfield.name).toBe("Spiderman Sony");
       expect(andrewGarfield.age).toBe(31);
       expect(andrewGarfield.actor).toBe("Andrew Garfield");
-      expect(andrewGarfield.movies).toBe(31);
+      expect(andrewGarfield.movies).toBe(2);
       expect(andrewGarfield.studio).toBe("Sony");
       
     });
